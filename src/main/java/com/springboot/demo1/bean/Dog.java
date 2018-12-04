1584c02edbea3e660ad081e0de77f018c08b2572
LiuShunBao <1094710983@qq.com>
2018-12-04 17:43:15 +0800

    111

---
+++ src/main/java/com/springboot/demo1/bean/Dog.java
@@ -0,0 +1,29 @@
+package com.springboot.demo1.bean;
+
+/**
+ * @Description:
+ * @Author: bao
+ * @CreateDate: 2018/11/27 0027 下午 3:35
+ */
+public class Dog {
+
+    private String name;
+
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
